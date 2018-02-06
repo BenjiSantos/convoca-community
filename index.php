232f51fe422005a18eca824ca7720f521dd6ca69
Benji Santos <Benji@programemos.local>
2018-02-05 19:03:51 -0500

    Agregando estructura sin estilos

--- index.php
+++ index.php
@@ -1,1 +1,197 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CONVOCA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="iconmoon/style.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="padding: 5px 15px;">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="" href="http://convoca.pe/">
+                <img height="30" src="https://investigalavajato.convoca.pe/public/v3/assets/imgs/convoca_logo.png">
+                <img height="30" src="http://conboca.pe/img/logo-conboca.png">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="/">Quiénes somos</a></li>
+                <li><button class="btn btn-danger">Haz tu aporte</button></li>
+                <li class=""><a href="/mapa">Cómo involucrarte</a></li>
+                <li class=""><a href="/#">Agenda</a></li>
+                <li><a href="#inicio"><span class="icon-home"></span></a></li>
+                <li><a href="javascript:var dir=window.document.URL;var tit=window.document.title;var tit2=encodeURIComponent(tit);var dir2= encodeURIComponent(dir);window.open('http://www.facebook.com/share.php?u='+dir2+'&amp;t='+tit2+'','ventana','width=600,height=400,left=50,top=50,toolbar=yes');void 0"><span class="icon-facebook"></span></a></li>
+                <li><a href="javascript:var dir=window.document.URL;var tit=window.document.title;var tit2=encodeURIComponent(tit);window.open('http://twitter.com/?status='+tit2+'%20'+dir+'','ventana','width=600,height=400,left=50,top=50,toolbar=yes');void 0"><span class="icon-twitter"></span></a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<section id="banner-home">
+    <div class="container-fluid">
+        <div class="row background-img">
+            <div class="col-md-3">
+
+            </div>
+            <div class="col-md-6 pad-banner">
+                <h1>Comunidad ConBoca</h1>
+                <p>Un espacio para los ciudadanos que buscan intercambiar información y expandir historias
+                de gran impacto para llegar a quienes nos gobiernan</p>
+                <br>
+                <button class="btn btn-danger">Explorar</button>
+            </div>
+            <div class="col-md-3">
+
+            </div>
+        </div>
+    </div>
+</section>
+<section id="about">
+    <div class="container-fluid background-img">
+        <div class="container">
+            <h1>¿Quiénes somos?</h1>
+        </div>
+    </div>
+    <div class="container">
+        <p>Convoca es un medio digital de periodismo de investigación</p>
+        <p>Convoca es un medio digital de periodismo de investigación</p>
+    </div>
+</section>
+<section id="our-work">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-1 background-img">
+
+            </div>
+            <div class="col-md-11 background-img">
+                <h1>Nuestro trabajo</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+
+                <div class="item active">
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://www.convoca.pe/sites/default/files/styles/full_post/public/yukimori.jpg?itok=Q9YL_8yP" alt="Los Angeles">
+                    </div>
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://memoriasdelacorrupcion.convoca.pe/img/investiga-lavajato.jpg" alt="Los Angeles">
+                    </div>
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://ilusionfiscal.convoca.pe/img/header.jpg" alt="Los Angeles">
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://convoca.pe/especiales/juegosdelpoder/img/PUERTA-ABIERTA-min.jpg" alt="Los Angeles">
+                    </div>
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://www.convoca.pe/especiales/patrimonio-sa/img/img_congreso.jpg" alt="Los Angeles">
+                    </div>
+                    <div class="col-md-4 item-carousel-convoca conta">
+                        <img class="image" src="http://convoca.pe/especiales/crisis-presidencial/img/ppk_uno.jpg" alt="Los Angeles">
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control back-none" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control back-none" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</section>
+<section id="aporte">
+    <div class="container-fluid background-img">
+        <div class="container">
+            <h1></h1>
+        </div>
+    </div>
+</section>
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-sm-12">
+                    <p class="text-center">
+                        <br><br class="hidden-xs"><br class="hidden-xs">
+                        <a href="http://convoca.pe/">
+                            <img src="https://investigalavajato.convoca.pe/public/v3/assets/imgs/logo_convoca_color.png?v1" alt="" class="img-responsive">
+                        </a>
+                    </p>
+
+                </div>
+            </div>
+            <p class="text-center hidden-xs hidden-sm publicado">
+                Convoca © 2017 Todos los derechos reservados
+            </p>
+        </div>
+        <div class="col-md-5 publicado">
+            <div class="row">
+                <div class="col-xs-8 col-xs-offset-2 col-sm-12">
+                    <p>
+                        <br class="hidden-xs">
+                        <span>Responsable del proyecto: Milagros Salazar. <br>
+                  Reporteros: Óscar Libón<br>
+                  Gonzalo Torrico, Claudia Martorellet. <br>
+                  Miguel Gutiérrez, Aramís Castro. <br>
+                  Diseño, ilustraciones y vídeos: Orlando Tapia <br>
+                  Podcast: Karla Velezomoro, Sonia Tello, Michelle Mogollón y Walter Dávila. <br>
+                  Desarrollo web: Benji Santos.
+                </span>
+                        <br><br><br class="hidden-xs"><br class="hidden-xs">
+                        <span>
+                  Si tienes alguna denuncia o quieres contactarte<br> con nosotros búscanos en info@convoca.pe
+                </span>
+                        <br clear="visible-xs"><br clear="visible-xs">
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-5">
+            <div class="row">
+                <div class="col-xs-6 col-xs-offset-3 col-sm-12 col-sm-offset-0">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <br class="hidden-xs">
+                            <font color="red">
+                            </font><p class="menu"><font color="red">
+                                    <a href="http://convoca.pe/investigaciones" target="_blank"><font color="#EEA031">Investigaciones</font></a><br>
+                                    <a href="http://convoca.pe/agenda-propia" target="_blank"><font color="#EEA031">Agenda Propia</font></a><br>
+                                    <a href="http://convoca.pe/mapea" target="_blank"><font color="#EEA031">Apps</font></a><br>
+                                    <a href="http://convoca.pe/opiniones" target="_blank"><font color="#EEA031">Opinión</font></a><br>
+                                    <a href="http://convoca.pe/convoca-radio" target="_blank"><font color="#EEA031">Convoca Radio</font></a></font>
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>
+                                <br class="hidden-xs">
+                                Convoca es miembro de:
+                                <img src="http://www.convoca.pe/sites/all/themes/theme930/images/GIJN_150x150.jpg" alt="" class="img-responsive">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
